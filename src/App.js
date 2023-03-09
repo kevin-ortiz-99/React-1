914f165460c3355c7ac43e02fe6c0fdc8e124144
kevin-ortiz-99 <112679542+kevin-ortiz-99@users.noreply.github.com>
2023-03-08 16:37:28 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
 import React, { Component } from 'react';
+import Header from './Componentes/Header';
 import Banner from './Componentes/Banner';
 import Body from './Componentes/Body';
 import Footer from './Componentes/Footer';
 
 
 class App extends Component {
-  render() {   
-  return (
-    <div className="App">
-      <h1>Hola</h1>
-      <Banner 
-        texto="Texto 1"
-      />
-      <Banner 
-        texto="Texto 2"
-      />
-      <Body 
-        text="Texto 3" arreglo={[1,2,3,4,5]}
-      />
-    
+  render() {
+    return (
+      <div className="App">
+        <Banner
+          texto="Texto 1"
+        />
+
+        <Header />
+
+        <Banner
+          texto="Texto 2"
+        />
+
+        <div className="espacio"> </div>
+        <Body
+          text="Texto 3" arreglo={[1, 2, 3, 4, 5]}
+        />
+
+        <Footer>
+          texto 4
+        </Footer>
+
       </div>
     );
   }
